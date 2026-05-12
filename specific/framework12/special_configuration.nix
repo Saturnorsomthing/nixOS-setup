2ec67c5774e096e0da0a06c3bc713c052a597cd4
Saturnorsomthing <aus72265@pm.me>
2026-05-12 21:14:33 +0200

    framework 12 fix

--- specific/framework12/special_configuration.nix
+++ specific/framework12/special_configuration.nix
@@ -1,13 +1,11 @@
 { config, pkgs, lib, ... }:
 
 {
-  networking.hostName = "nixos-fw12";
+  networking.hostName = "nixos";
 
-  # Intel Specific Boot
   boot.initrd.kernelModules = [ "i915" "thunderbolt" ];
   boot.kernelParams = [ "mem_sleep_default=deep" "i915.enable_psr=1" ];
 
-  # Graphics Drivers (Fixes Zig EGLInitFailed)
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
@@ -18,32 +16,39 @@
     ];
   };
 
-  # Rotation & Sensors
   hardware.sensor.iio.enable = true;
   services.udev.packages = with pkgs; [ iio-sensor-proxy ];
-  
+
+  services.fwupd.enable = true;
+  services.fprintd.enable = true;
+  services.thermald.enable = true;
+
   systemd.user.services.niri-rotation = {
     description = "Niri Orientation Loop";
     wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
     serviceConfig = {
       ExecStart = pkgs.writeShellScript "niri-rotator" ''
-        ${pkgs.iio-sensor-proxy}/bin/monitor-sensor | while read -r line; do
-          if echo "$line" | grep -q "orientation changed"; then
-            case "$line" in
-              *"normal"*)    ${pkgs.niri}/bin/niri msg output eDP-1 transform normal ;;
-              *"bottom-up"*) ${pkgs.niri}/bin/niri msg output eDP-1 transform 180 ;;
-              *"left-up"*)   ${pkgs.niri}/bin/niri msg output eDP-1 transform 90 ;;
-              *"right-up"*)  ${pkgs.niri}/bin/niri msg output eDP-1 transform 270 ;;
-            esac
-          fi
+        ${pkgs.coreutils}/bin/stdbuf -oL ${pkgs.iio-sensor-proxy}/bin/monitor-sensor | while read -r line; do
+          case "$line" in
+            *"normal"*)    ${pkgs.niri}/bin/niri msg output eDP-1 transform normal ;;
+            *"bottom-up"*) ${pkgs.niri}/bin/niri msg output eDP-1 transform 180 ;;
+            *"left-up"*)   ${pkgs.niri}/bin/niri msg output eDP-1 transform 90 ;;
+            *"right-up"*)  ${pkgs.niri}/bin/niri msg output eDP-1 transform 270 ;;
+          esac
         done
       '';
       Restart = "always";
-      Environment = [ "WAYLAND_DISPLAY=wayland-0" "XDG_RUNTIME_DIR=/run/user/1000" ];
+      Environment = [ "WAYLAND_DISPLAY=wayland-0" ];
     };
   };
 
+  environment.systemPackages = with pkgs; [
+    awww
+  ];
+
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = lib.mkForce "iHD";
+    __GLX_VENDOR_LIBRARY_NAME = lib.mkForce "mesa";
   };
 }
