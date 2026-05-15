04b6be4a37ea59da0dc49f6b64b2b6babc442720
Saturnorsomthing <aus72265@pm.me>
2026-05-15 15:56:17 +0200

    feat: framework 13 upgrade

--- specific/framework13/special_configuration.nix
+++ specific/framework13/special_configuration.nix
@@ -1,25 +1,39 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   networking.hostName = "nixos";
 
   boot.kernelParams = [ "amd_pstate=active" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
-  
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       libGL
+      libva-vdpau-driver
+      libvdpau-va-gl
     ];
   };
 
-  environment.systemPackages = with pkgs; [
-    awww
-  ];
+  services.fprintd.enable = true;
+  security.pam.services.login.fprintAuth = lib.mkForce true;
+  security.pam.services.sudo.fprintAuth = lib.mkForce true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+    };
+  };
 
   services.fwupd.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
-  
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+
+  environment.systemPackages = with pkgs; [
+    awww
+    libfprint
+  ];
 }
