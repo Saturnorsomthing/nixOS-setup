5e1dda7a75103b89c8bba37b2f0175b449bf20c8
Saturnorsomthing <aus72265@pm.me>
2026-05-16 16:57:30 +0200

    feat: framework 13 fix

--- specific/framework13/special_configuration.nix
+++ specific/framework13/special_configuration.nix
@@ -13,6 +13,7 @@
       libGL
       libva-vdpau-driver
       libvdpau-va-gl
+      mesa
     ];
   };
 
@@ -20,13 +21,23 @@
   security.pam.services.login.fprintAuth = lib.mkForce true;
   security.pam.services.sudo.fprintAuth = lib.mkForce true;
 
+  services.thermald.enable = true;
   services.tlp = {
     enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      PLATFORM_PROFILE_ON_BAT = "low-power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
+      RUNTIME_PM_ON_AC = "on";
+      RUNTIME_PM_ON_BAT = "auto";
+      WIFI_PWR_ON_AC = "off";
+      WIFI_PWR_ON_BAT = "on";
+      NMI_WATCHDOG = 0;
     };
   };
 
